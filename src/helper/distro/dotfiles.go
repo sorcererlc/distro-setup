636e23ca99242665031acda40e228e5aa72caab3
Cristian Toma <cristi@cristiant.com>
2024-08-29 20:01:44 +0300

    Update dotfiles setup.

--- src/helper/distro/dotfiles.go
+++ src/helper/distro/dotfiles.go
@@ -8,13 +8,19 @@ import (
 func (f *DistroHelper) setupDotfiles(p types.GitPackage) error {
 	f.Log.Info("Setting up dotfiles")
 
+	err := helper.Run("rm", "-rf", "../dotfiles")
+	if err != nil {
+		f.Log.Error("Remove existing dotfiles repo", err.Error())
+		return err
+	}
+
 	args := []string{"git", "clone", "--depth", "1"}
 	if p.Tag != "" {
 		args = append(args, "--branch", p.Tag)
 	}
 	args = append(args, p.Url, "../dotfiles")
 
-	err := helper.Run(args...)
+	err = helper.Run(args...)
 	if err != nil {
 		f.Log.Error("Clone dotfiles repo", err.Error())
 		return err
