97b5db636df671d9ffc7ac3d72f58d00ad6f6826
Cristian Toma <cristi@cristiant.com>
2024-08-30 17:51:49 +0300

    Automate sensors-detect.

--- src/helper/distro/sensors.go
+++ src/helper/distro/sensors.go
@@ -9,7 +9,7 @@ func (f *DistroHelper) detectSensors() error {
 
 	f.Log.Info("Detecting hardware sensors")
 
-	err := helper.Run("sudo", "sensors-detect")
+	err := helper.Run("sudo", "sensors-detect", "--auto")
 	if err != nil {
 		f.Log.Error("Sensors detect", err.Error())
 		return err
